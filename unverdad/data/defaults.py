9b13bdadc2ef9953120177d8cc7a95897545a61c
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2024-05-21 16:34:56 -0500

    Add guilty gear strive game default

---
+++ unverdad/data/defaults.py
@@ -0,0 +1,17 @@
+"""Default entities"""
+
+import pathlib
+
+from unverdad.data import database, tables
+
+
+def game_defaults() -> list[tables.game.GameEntity]:
+    return [
+        tables.game.GameEntity(
+            game_id=database._new_uuid(),
+            gb_game_id=None,
+            name="Guilty Gear Strive",
+            game_path_offset=pathlib.Path("RED/Content/Paks/"),
+            mods_home_relative_path=pathlib.Path("~mods/"),
+        )
+    ]
