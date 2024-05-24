b75aacd547ec0a57d89b9af03aab60de3322504a
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2024-05-24 14:43:49 -0500

    Refactor constraint/validation to sql

--- unverdad/data/tables/pak.py
+++ unverdad/data/tables/pak.py
@@ -6,45 +6,10 @@ Module level function are for manipulating the table.
 """
 
 import dataclasses
-import enum
 import pathlib
 import uuid
-from typing import Optional
 
 TABLE_NAME = "pak"
-
-
-class PakError(enum.Enum):
-    """Error types used by PakReport."""
-
-    STEM_MISMATCH = enum.auto()
-    PAK_WRONG_EXTENSION = enum.auto()
-    SIG_WRONG_EXTENSION = enum.auto()
-
-
-class PakReport:
-    """Collection of errors, if any, found during pak validation."""
-
-    def __init__(self):
-        self.errors = []
-
-    def add_error(self, error: PakError):
-        self.errors.append(error)
-
-    def is_good(self) -> bool:
-        return len(self.errors) == 0
-
-    def to_exception(self) -> Optional[Exception]:
-        if self.is_good():
-            return None
-        msg = " ".join([f"{e}" for e in self.errors])
-        return Exception(msg)
-
-    def good_or_raise(self):
-        e = self.to_exception()
-        if e is None:
-            return self
-        raise e
 
 
 @dataclasses.dataclass
@@ -53,27 +18,14 @@ class PakEntity:
     Attributes:
         pak_id: local id
         mod_id: associated mod id
-        pak_path: filepath of .pak, relative to mod home
-        sig_path: filepath of .sig, relative to mod home
+        pak_path: filepath of .pak, relative to parent mod
+        sig_path: filepath of .sig, relative to parent mod
     """
 
     pak_id: uuid.UUID
     mod_id: uuid.UUID
     pak_path: pathlib.Path
     sig_path: pathlib.Path
-
-    def _params(self):
-        return dataclasses.asdict(self)
-
-    def validate(self) -> PakReport:
-        report = PakReport()
-        if self.pak_path.suffix != ".pak":
-            report.add_error(PakError.PAK_WRONG_EXTENSION)
-        if self.sig_path.suffix != ".sig":
-            report.add_error(PakError.SIG_WRONG_EXTENSION)
-        if self.pak_path.stem != self.sig_path.stem:
-            report.add_error(PakError.STEM_MISMATCH)
-        return report
 
 
 def create_table(con):
@@ -87,8 +39,8 @@ def create_table(con):
 CREATE TABLE IF NOT EXISTS pak (
     pak_id uuid NOT NULL PRIMARY KEY,
     mod_id uuid NOT NULL,
-    pak_path path NOT NULL,
-    sig_path path NOT NULL,
+    pak_path path NOT NULL CHECK(pak_path LIKE '%.pak'),
+    sig_path path NOT NULL CHECK(sig_path LIKE '%.sig'),
     FOREIGN KEY (mod_id)
     REFERENCES mod (mod_id)
         ON UPDATE CASCADE
@@ -100,7 +52,7 @@ CREATE TABLE IF NOT EXISTS pak (
 
 def insert_many(con, data: list[PakEntity]):
     """Insert each of data into pak table."""
-    d = [x._params() for x in data]
+    d = [dataclasses.asdict(x) for x in data]
     with con:
         con.executemany(
             """
