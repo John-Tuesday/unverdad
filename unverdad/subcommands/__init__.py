9561c3403875d36809d4abbe1d64e5ef35a4af0a
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2024-05-25 00:24:56 -0500

    Remove out dated and bad command

--- unverdad/subcommands/__init__.py
+++ unverdad/subcommands/__init__.py
@@ -4,9 +4,9 @@ Designed to be imported under a namespace.
     example: `from unverdad import subcommands`
 """
 
-from unverdad.subcommands import config, import_mods, info, install, mod_registry
+from unverdad.subcommands import config, import_mods, install, mod_registry
 
 
 def as_list():
     """Return a new list of all subcommand modules."""
-    return [info, config, import_mods, install, mod_registry]
+    return [config, import_mods, install, mod_registry]
