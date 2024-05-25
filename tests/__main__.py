73456a0f96a9a1bcbdc23d515fcaeab3c4479a3c
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2024-05-25 00:27:51 -0500

    Runs manual tests on sample data in memory

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,11 +1,14 @@
+import unverdad
 from tests.data import fixture_database as fd
 from unverdad.data import database
 
 
 def main():
     con = database._reset_db(db_path=None)
-    fd.print_all(con)
+    print("IN MEMORY DATABASE")
     fd.insert_samples(con)
+    print("TEST SAMPLES IN USE")
+    unverdad.main()
     fd.print_all(con)
 
 
