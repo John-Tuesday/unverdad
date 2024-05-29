41949b36751af1c7c13a43082e8455da9fe7cbf4
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2024-05-29 05:30:10 -0500

    Reformat

--- unverdad/subcommands/config.py
+++ unverdad/subcommands/config.py
@@ -1,39 +1,50 @@
 import argparse
 import logging
+
 from unverdad.config import user_config
+
 logger = logging.getLogger(__name__)
+
 
 def attach(subparsers) -> argparse.ArgumentParser:
     parser = subparsers.add_parser(
-        'config',
-        help='interact with current config',
-        description='query/set/verify config values')
+        "config",
+        help="interact with current config",
+        description="query/set/verify config values",
+    )
     g = parser.add_mutually_exclusive_group()
     g.add_argument(
-        '-l', '--list-all', 
-        help='list all config key-value pairs',
-        action="store_true")
+        "-l",
+        "--list-all",
+        help="list all config key-value pairs",
+        action="store_true",
+    )
     g.add_argument(
-        '-g', '--get', 
-        help='get config value from key',
-        action="extend", nargs='+',
-        dest='keys', metavar='KEY')
+        "-g",
+        "--get",
+        help="get config value from key",
+        action="extend",
+        nargs="+",
+        dest="keys",
+        metavar="KEY",
+    )
     g.add_argument(
-        '--verify',
-        help='verify config file is valid',
-        action='store_true')
+        "--verify",
+        help="verify config file is valid",
+        action="store_true",
+    )
     return parser
 
+
 def hook(args):
-    logger.info('config')
+    logger.info("config")
     if args.keys:
-        logger.info('get config value by one or more keys')
+        logger.info("get config value by one or more keys")
         value = user_config.SCHEMA.format_export_keys(args.config, *args.keys)
         logger.info(value)
     elif args.list_all:
-        logger.info('list all config options')
-        logger.info(f'{args.config}')
+        logger.info("list all config options")
+        logger.info(f"{args.config}")
     elif args.verify:
-        logger.info('verify config')
-        logger.info('verified ...')
-
+        logger.info("verify config")
+        logger.info("verified ...")
