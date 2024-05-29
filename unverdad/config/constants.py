65392e04205a622647bebe1ed5741cb7c3851419
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2024-05-29 06:13:43 -0500

    Refactor with better types

--- unverdad/config/constants.py
+++ unverdad/config/constants.py
@@ -1,24 +1,28 @@
 import os
 import pathlib
 
-APP_NAME = "unverdad"
-__version__ = "0.0.0"
-APP_VERSION = __version__
+__version__: str = "0.0.0"
+
+APP_NAME: str = "unverdad"
+APP_VERSION: str = __version__
 STAGE: str = "production"
 
-DATA_HOME = (
-    pathlib.Path(os.getenv("XDG_DATA_HOME", "~/.local/share")).expanduser() / APP_NAME
-)
-CONFIG_HOME = (
-    pathlib.Path(os.getenv("XDG_CONFIG_HOME", "~/.config")).expanduser() / APP_NAME
-)
-STATE_HOME = (
-    pathlib.Path(os.getenv("XDG_STATE_HOME", "~/.local/state")).expanduser() / APP_NAME
-)
+DATA_HOME = pathlib.Path(
+    os.getenv("XDG_DATA_HOME", "~/.local/share"),
+    APP_NAME,
+).expanduser()
+CONFIG_HOME = pathlib.Path(
+    os.getenv("XDG_CONFIG_HOME", "~/.config"),
+    APP_NAME,
+).expanduser()
+STATE_HOME = pathlib.Path(
+    os.getenv("XDG_STATE_HOME", "~/.local/state"),
+    APP_NAME,
+).expanduser()
 
-LOG_FILE = STATE_HOME / "log"
-CONFIG_FILE = CONFIG_HOME / "config.toml"
-DB_FILE = DATA_HOME / "db"
+LOG_FILE: pathlib.Path = STATE_HOME / "log"
+CONFIG_FILE: pathlib.Path = CONFIG_HOME / "config.toml"
+DB_FILE: pathlib.Path = DATA_HOME / "db"
 
 # Ensure directory homes exist
 for dir in [DATA_HOME, CONFIG_HOME, STATE_HOME]:
