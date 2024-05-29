035c236bffb9d685659fb1bf4a3bc65f959683e1
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2024-05-29 09:46:57 -0500

    Refactor settings values
    Remove outdated global `install_dir`.
    Add `games` table to configure predefined games, namely "Guilty Gear
    Strive".

--- unverdad/config/user_config.py
+++ unverdad/config/user_config.py
@@ -1,4 +1,5 @@
 import pathlib
+import tomllib
 
 from unverdad.config import constants, schema
 
@@ -6,18 +7,24 @@ from unverdad.config import constants, schema
 def __config_schema() -> schema.Schema:
     root = schema.Schema(description="config for app")
     root.add_item(
-        name="install_dir",
-        possible_values=[schema.SchemaValue.path_schema()],
-        default=pathlib.Path(
-            "~/.steam/root/steamapps/common/GUILTY GEAR STRIVE/RED/Content/Paks/~mods"
-        ),
-        description="mods installation destination.",
-    )
-    root.add_item(
-        name="mods_dir",
+        name="mods_home",
         possible_values=[schema.SchemaValue.path_schema()],
         default=constants.DATA_HOME / "mods",
         description="mods import destination.",
+    )
+    game_table = root.add_subtable(
+        name="games",
+        description="table of game settings where each game is all lowercase",
+    )
+    ggs_table = game_table.add_subtable(
+        name="guilty_gear_strive",
+        description="GUILTY GEAR STRIVE options",
+    )
+    ggs_table.add_item(
+        name="game_path",
+        possible_values=[schema.SchemaValue.path_schema()],
+        default=pathlib.Path("~/.steam/root/steamapps/common/GUILTY GEAR STRIVE/"),
+        description="game install path.",
     )
     return root
 
