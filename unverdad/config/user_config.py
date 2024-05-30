939bc3895a958d8e5f491be9f22d0e34474c42f9
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2024-05-30 10:07:45 -0500

    Update schemaspec and use new Spec builder

--- unverdad/config/user_config.py
+++ unverdad/config/user_config.py
@@ -1,49 +1,72 @@
+import dataclasses
 import pathlib
 
 import schemaspec
 from unverdad.config import constants
 
 
-def __config_schema() -> schemaspec.Schema:
-    root = schemaspec.Schema(description="config for app")
-    root.add_item(
-        name="mods_home",
-        possible_values=[schemaspec.SchemaValue.path_schema()],
+@dataclasses.dataclass
+class SettingsSpec:
+    mods_home: pathlib.Path = dataclasses.field(
         default=constants.DATA_HOME / "mods",
-        description="mods import destination.",
+        metadata=schemaspec.SchemaItemField(
+            possible_values=[schemaspec.PathSchema()],
+            description="mods import destination.",
+        ).metadata(),
     )
-    default_game_table = root.add_subtable(
-        name="default_game",
-        description="table of game settings where each game is all lowercase",
+
+    @dataclasses.dataclass
+    class DefaultGameSpec:
+        name: str = dataclasses.field(
+            default="Guilty Gear Strive",
+            metadata=schemaspec.SchemaItemField(
+                possible_values=[schemaspec.StringSchema()],
+                description="name of the game",
+            ).metadata(),
+        )
+        enabled: bool = dataclasses.field(
+            default=True,
+            metadata=schemaspec.SchemaItemField(
+                possible_values=[schemaspec.BoolSchema()],
+                description="whether or not default_game should be used at all.",
+            ).metadata(),
+        )
+
+    default_game: DefaultGameSpec = dataclasses.field(
+        default_factory=DefaultGameSpec,
+        metadata=schemaspec.SchemaTableField(
+            description="table of game settings where each game is all lowercase",
+        ).metadata(),
     )
-    default_game_table.add_item(
-        name="name",
-        possible_values=[schemaspec.SchemaValue.str_schema()],
-        default="guilty gear strive",
-        description="name of game",
+
+    @dataclasses.dataclass
+    class PredefinedGamesSpec:
+        @dataclasses.dataclass
+        class GameSpec:
+            game_path: pathlib.Path = dataclasses.field(
+                default=pathlib.Path(
+                    "~/.steam/root/steamapps/common/GUILTY GEAR STRIVE/"
+                ),
+                metadata=schemaspec.SchemaItemField(
+                    possible_values=[schemaspec.PathSchema()],
+                    description="game install path.",
+                ).metadata(),
+            )
+
+        guilty_gear_strive: GameSpec = dataclasses.field(
+            default_factory=GameSpec,
+            metadata=schemaspec.SchemaTableField(
+                description="GUILTY GEAR STRIVE options",
+            ).metadata(),
+        )
+
+    games: PredefinedGamesSpec = dataclasses.field(
+        default_factory=PredefinedGamesSpec,
+        metadata=schemaspec.SchemaTableField(
+            description="table of game settings where each game is all lowercase",
+        ).metadata(),
     )
-    default_game_table.add_item(
-        name="enabled",
-        possible_values=[schemaspec.SchemaValue.bool_schema()],
-        default=True,
-        description="whether or not default_game should be used at all.",
-    )
-    game_table = root.add_subtable(
-        name="games",
-        description="table of game settings where each game is all lowercase",
-    )
-    ggs_table = game_table.add_subtable(
-        name="guilty_gear_strive",
-        description="GUILTY GEAR STRIVE options",
-    )
-    ggs_table.add_item(
-        name="game_path",
-        possible_values=[schemaspec.SchemaValue.path_schema()],
-        default=pathlib.Path("~/.steam/root/steamapps/common/GUILTY GEAR STRIVE/"),
-        description="game install path.",
-    )
-    return root
 
 
-SCHEMA = __config_schema()
-SETTINGS = SCHEMA.load_toml(constants.CONFIG_FILE)
+SCHEMA = schemaspec.schema_from(SettingsSpec)
+SETTINGS = SCHEMA.load_toml(constants.CONFIG_FILE, namespace=SettingsSpec())
